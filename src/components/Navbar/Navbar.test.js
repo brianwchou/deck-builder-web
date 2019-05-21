b87eb8c5ff473451c30a918fed2ea4eeae1aa6f1
zhi <zhc1289@gmail.com>
2019-05-21 18:47:30 -0400

    added testing for Navbar

---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from 'components/Navbar'
+import renderer from 'react-test-renderer'
+
+describe('Navbar', () => {
+
+    test('renders correctly', () => {
+        const tree = renderer
+            .create(<Navbar />)
+            .toJSON();
+         
+        expect(tree).toMatchSnapshot();    
+    })
+})
