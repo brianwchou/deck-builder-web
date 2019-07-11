61a2938eacfcf37cd38813e95deb59fcd882cb55
zhi <zhc1289@gmail.com>
2019-07-11 18:49:41 -0400

    converted DeckListEntry to typescript, added interface file

--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -1,35 +1,12 @@
 import React from 'react';
 import Graph from './Graph'
+import { CardInfo } from '../../common/types';
 
 const metricsStyle = {
     border: 'solid black thin',
     width: '35vw',
     height: '30vh',
     display: 'inline-block'
-}
-
-interface CardInfo {
-    artist: string,
-    cmc: number,
-    color_identity: Array<string>,
-    colors: Array<string>,
-    image_uris: {
-        art_crop: string,
-        border_crop: string,
-        large: string,
-        normal: string,
-        png: string,
-        small: string
-    },
-    mana_cost: string,
-    name: string,
-    oracle_text: string,
-    power: string,
-    rarity: string,
-    reserved: boolean,
-    setName: string,
-    toughness: string,
-    typeLine: string,
 }
 
 interface CardCount {
