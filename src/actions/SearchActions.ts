e2a3372f236c4a548b31393ac13885f67004c391
brian <bchou515@gmail.com>
2019-08-12 16:32:20 -0400

    added transform for search card

--- src/actions/SearchActions.ts
+++ src/actions/SearchActions.ts
@@ -33,29 +33,41 @@ export const getCardSearchData = (url: string) => {
         })
         .then((json) => {
           if (json) {
-            var cards = json.data.filter((info: any) => {
-              return info.layout === "normal"
-            })
+            var cards = json.data
             .map((info: any) => {
-                return {
-                  artist: info.artist,
-                  cmc: info.cmc,
-                  color_identity: info.color_identity,
-                  colors: info.colors,
-                  image_uris: info.image_uris,
-                  layout: info.layout,
-                  mana_cost: info.mana_cost,
-                  name: info.name,
-                  oracle_text: info.oracle_text,
-                  power: info.power,
-                  rarity: info.rarity,
-                  related_cards: info.all_parts,
-                  reserved: info.reserved,
-                  setName: info.setName,
-                  toughness: info.toughness,
-                  transform_sides: info.card_faces,
-                  typeLine: info.type_line,
+                let cardAttributes = {
+                    artist: info.artist,
+                    cmc: info.cmc,
+                    color_identity: info.color_identity,
+                    colors: info.colors,
+                    image_uris: info.image_uris,
+                    layout: info.layout,
+                    mana_cost: info.mana_cost,
+                    name: info.name,
+                    oracle_text: info.oracle_text,
+                    power: info.power,
+                    rarity: info.rarity,
+                    related_cards: info.all_parts,
+                    reserved: info.reserved,
+                    setName: info.setName,
+                    toughness: info.toughness,
+                    transform_sides: info.card_faces,
+                    typeLine: info.type_line,
                 }
+                
+                if (info.layout === 'transform') {
+                    let transformAttributes = {
+                        colors: info.card_faces[0].colors,
+                        image_uris: [info.card_faces[0].image_uris, info.card_faces[1].image_uris],
+                        mana_cost: info.card_faces[0].mana_cost,
+                        oracle_text: [info.card_faces[0].orcale_text, info.card_faces[1].orcale_text],
+                        power: [info.card_faces[0].power, info.card_faces[1].power],
+                        toughness: [info.card_faces[0].toughness, info.card_faces[1].toughness],
+                        typeLine: [info.card_faces[0].typeLine, info.card_faces[1].typeLine]
+                    }
+                    cardAttributes = Object.assign({}, cardAttributes, transformAttributes)
+                }
+                return cardAttributes
             })
             console.log(cards)
             dispatch(loadSearchCards(cards));
