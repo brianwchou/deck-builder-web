7b145264026c3daf4811b778d4831f774776e397
zhi <zhc1289@gmail.com>
2019-08-26 18:40:32 -0400

    fixed typo in home path

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar () {
     return (
         <nav>
             <ul className = "nav">
-                <li className = "horizontal"><a className = "changeColor" href="Home">Home</a></li>
+                <li className = "horizontal"><a className = "changeColor" href="home">Home</a></li>
             </ul>
         </nav>
     )
