6a8d9d570e2a3e80a0e789deb980a0f03e341e38
brian <bchou515@gmail.com>
2019-09-05 17:25:27 -0400

    auto commit [ci skip]

---
+++ precache-manifest.90c3747d9442f4d6dfea2a73d7f88aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3100395852954951178af465081415cb",
+    "url": "/deck-builder-web/index.html"
+  },
+  {
+    "revision": "2aff3ccf70ba80cb56fa",
+    "url": "/deck-builder-web/static/css/main.4a2643eb.chunk.css"
+  },
+  {
+    "revision": "9b11797198ffb301b2e5",
+    "url": "/deck-builder-web/static/js/2.16a8e5e8.chunk.js"
+  },
+  {
+    "revision": "2aff3ccf70ba80cb56fa",
+    "url": "/deck-builder-web/static/js/main.384ec863.chunk.js"
+  },
+  {
+    "revision": "db88c4a0dd509fa90d6c",
+    "url": "/deck-builder-web/static/js/runtime~main.04fec7b4.js"
+  }
+]);
