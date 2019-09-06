f0cab63a51a685f2c609b141e19307d2988b7320
brian <bchou515@gmail.com>
2019-09-05 22:31:50 -0400

    remove a couple imports

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from '../../components/Navbar'
 import NoMatch from '../../components/NoMatch';
