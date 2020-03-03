68dd78f2d74b49c621b46bf80a65e0d35a05baee
brian <bchou515@gmail.com>
2020-03-02 22:22:32 -0500

    grid updates

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Navbar from '../../components/Navbar';
 import NoMatch from '../../components/NoMatch';
 import DeckBuilder from '../../containers/DeckBuilder';
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <BrowserRouter basename="/deck-builder-web/">
-        <Switch>
-          <Route exact path="/" component={DeckBuilder} />
-          <Route component={NoMatch} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column>
+          <Navbar />
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column>
+          <BrowserRouter>
+          <Switch>
+            <Route exact path="/home" component={DeckBuilder} />
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route component = {NoMatch}/>
+          </Switch>
+          </BrowserRouter>
+          </Grid.Column>
+      </Grid.Row>  
+    </Grid>
   )
 }
+
 
 export default App;
