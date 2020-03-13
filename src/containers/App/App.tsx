036556d933c4acafa12229755cf50d14869a634e
brian <bchou515@gmail.com>
2020-03-12 23:31:46 -0400

    add new nav bar

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Navbar from '../../components/Navbar';
 import NoMatch from '../../components/NoMatch';
 import DeckBuilder from '../../containers/DeckBuilder';
 import React from 'react';
+import { AppBar, Toolbar, Box } from '@material-ui/core';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <Box width="100%" height="100%">
+      <AppBar position="static">
+        <Toolbar>
+          hello world
+        </Toolbar>
+      </AppBar>
       <BrowserRouter basename="/deck-builder-web/">
           <Switch>
             <Route exact path="/" component={DeckBuilder} />
             <Route component={NoMatch} />
           </Switch>
       </BrowserRouter>
-    </div>
+    </Box>
   )
 }
 
