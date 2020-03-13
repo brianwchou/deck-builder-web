1044f486c0571f61c12d87b07a0c6e0782b03943
brian <bchou515@gmail.com>
2020-03-12 23:31:12 -0400

    change margins

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 import rootReducer from './reducers'
+import './global.css'
 
 const store = createStore(
     rootReducer,
