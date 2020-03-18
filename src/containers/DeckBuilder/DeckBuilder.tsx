f20cac308ec6fbb9869a030b320cb535cab24162
brian <bchou515@gmail.com>
2020-03-18 00:54:26 -0400

    spacing between grid rows

--- src/containers/DeckBuilder/DeckBuilder.tsx
+++ src/containers/DeckBuilder/DeckBuilder.tsx
@@ -4,12 +4,20 @@ import DeckList from '../../containers/DeckList/DeckList';
 import SearchCardDisplay from '../../containers/SearchCardDisplay/SearchCardDisplay';
 import MaybeBoardDisplay from '../../containers/MaybeBoardDisplay/MaybeBoardDisplay';
 // import './DeckBuilder.css';
-import { Grid } from '@material-ui/core'
+import { Grid, withStyles } from '@material-ui/core'
 import Metrics from '../Metrics';
 
 const tag = <img className="background" src="https://cdn.arstechnica.net/wp-content/uploads/2016/01/159984_CN-980x597.jpg" alt="default"/>
 
-export default function DeckBuilder() {
+const styles = theme => ({
+  gridPadding: {
+    paddingTop: '20px'
+  }
+});
+
+function DeckBuilder(props) {
+  const { classes } = props;
+
   return (
     <Grid container>
       <Grid item xs={12}>
@@ -18,15 +26,17 @@ export default function DeckBuilder() {
       <Grid item xs={7}>
         <SearchCardDisplay />
       </Grid>
-      <Grid item xs = {4}>
+      <Grid item xs= {4}>
         <DeckList />
       </Grid>
-      <Grid item xs={7}>
+      <Grid className={classes.gridPadding} item xs={7}>
         <MaybeBoardDisplay />
       </Grid>
-      <Grid item xs = {4}>
+      <Grid className={classes.gridPadding} item xs= {4}>
         <Metrics />
       </Grid>
     </Grid>
   )
 }
+
+export default withStyles(styles)(DeckBuilder);
